aa6a5d91508198ff4cee6146a9d2421213520478
campos-kenjienishi <kenjicanpos829@gmail.com>
2026-03-06 17:49:51 +0800

    update terms link

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -79,8 +79,8 @@
                                     <!-- Terms Notice -->
                                     <div class="text-center mt-2 mb-3" style="font-size: 13px; color: #555;">
                                         By using this service, you understand and agree to the 
-                                        <a href="{{ url('/terms') }}" target="_blank">Terms of Use</a> and 
-                                        <a href="{{ url('/privacy') }}" target="_blank">Privacy Statement</a>.
+                                        <a href="https://www.pup.edu.ph/terms/" target="_blank">Terms of Use</a> and 
+                                        <a href="https://www.pup.edu.ph/privacy/" target="_blank">Privacy Statement</a>.
                                     </div>
                                     <br>
 
