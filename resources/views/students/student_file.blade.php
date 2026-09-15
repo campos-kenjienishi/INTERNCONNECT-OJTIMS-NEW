7a5b03e912ecfc4081679babb2b13fad86ea7fe5
campos-kenjienishi <kenjicanpos829@gmail.com>
2026-09-15 22:29:33 +0800

    Change downloadable files action buttons to square icon-only buttons

--- resources/views/students/student_file.blade.php
+++ resources/views/students/student_file.blade.php
@@ -241,17 +241,19 @@
                                         {{ $file->uploader_name ?: 'Coordinator' }}
                                     </td>
                                     <td>
-                                        <div class="action-btn-group">
+                                        <div class="actions-cell">
                                             <button type="button"
-                                                    class="btn-view btn-preview-file"
+                                                    class="icon-action-btn btn-view btn-preview-file"
+                                                    title="Preview File"
+                                                    aria-label="Preview File"
                                                     data-file-url="{{ url('/view/file', $file->file) }}"
                                                     data-file-name="{{ $file->name }}"
                                                     data-file-ext="{{ $ext }}"
                                                     data-download-url="{{ url('/download', $file->file) }}">
-                                                <i class="fa fa-eye"></i> View
+                                                <i class="fa fa-eye"></i>
                                             </button>
-                                            <a href="{{ url('/download', $file->file) }}" class="btn-download">
-                                                <i class="fa fa-download"></i> Download
+                                            <a href="{{ url('/download', $file->file) }}" class="icon-action-btn btn-download" title="Download File" aria-label="Download File">
+                                                <i class="fa fa-download"></i>
                                             </a>
                                         </div>
                                     </td>
@@ -380,17 +382,19 @@
                                                 {{ $file->uploader_name ?: $currentClass->adviser_name }}
                                             </td>
                                             <td>
-                                                <div class="action-btn-group">
+                                                <div class="actions-cell">
                                                     <button type="button"
-                                                            class="btn-view btn-preview-file"
+                                                            class="icon-action-btn btn-view btn-preview-file"
+                                                            title="Preview File"
+                                                            aria-label="Preview File"
                                                             data-file-url="{{ url('/view/file', $file->file) }}"
                                                             data-file-name="{{ $file->name }}"
                                                             data-file-ext="{{ $ext }}"
                                                             data-download-url="{{ url('/download', $file->file) }}">
-                                                        <i class="fa fa-eye"></i> View
+                                                        <i class="fa fa-eye"></i>
                                                     </button>
-                                                    <a href="{{ url('/download', $file->file) }}" class="btn-download">
-                                                        <i class="fa fa-download"></i> Download
+                                                    <a href="{{ url('/download', $file->file) }}" class="icon-action-btn btn-download" title="Download File" aria-label="Download File">
+                                                        <i class="fa fa-download"></i>
                                                     </a>
                                                 </div>
                                             </td>
